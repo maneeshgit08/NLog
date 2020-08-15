75d6715ecd045e8f5cade95ccc9682016cb4ee6a
Kabra, Manish (CT RDA DS AA DTS AU APPS) <manish.kabra@siemens.com>
2020-08-15 23:33:08 +0530

    first commit

---
+++ NLogExample/NLogExample/NLogTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NLog;
+
+namespace NLogExample
+{
+    [TestClass]
+    public class NLogTest
+    {
+        private Logger _logger = LogManager.GetCurrentClassLogger();
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            _logger.Trace("TestMethod1");
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            _logger.Debug("TestMethod2");
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            _logger.Info("TestMethod3");
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            _logger.Error("TestMethod4");
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            _logger.Fatal("TestMethod5");
+        }
+    }
+}
---
+++ NLogExample/NLogExample/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NLogExample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NLogExample")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("02c9f869-e504-4c8c-aa87-f99255ede596")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
